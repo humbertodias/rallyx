1c21cb5fce3c76be6d01fe942cf4ff16ab409de0
Humberto Dias <humberto.dias@ubisoft.com>
2020-07-01 09:52:39 +0200

    Cosmetic

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,6 +1,6 @@
 rm -rf /tmp/wwwroot && cp -r Game/wwwroot /tmp
-git checkout -f gh-pages
-rm -rf *  && cp -r /tmp/wwwroot/* .
+git checkout gh-pages
+rm -rf * && cp -r /tmp/wwwroot/* .
 git add *
 git commit -m "gh-pages"
 git push
