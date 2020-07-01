cae5867249899a3904234c551b4eda0b65a56d86
Humberto Dias <humberto.dias@ubisoft.com>
2020-07-01 09:26:27 +0200

    gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,4 +1,6 @@
-cp -r Game/wwwroot /tmp && rm -rf * && cp -r /tmp/wwwroot/* .
-git checkout gh-pages
+rm -rf /tmp/wwwroot && cp -r Game/wwwroot /tmp
+git checkout -f gh-pages
+rm -rf *  && cp -r /tmp/wwwroot/* .
+git add *
 git commit -m "gh-pages"
 git push
